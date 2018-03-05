549c4d88f0261dd195b2ee851f62602388c6efc3
Adam Levin <adamlevin@tutordelphia.com>
2018-03-05 13:42:39 -0500

    adds an app and renames the project

--- chess/settings.py
+++ chess/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'teacher.urls'
+ROOT_URLCONF = 'chess.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'teacher.wsgi.application'
+WSGI_APPLICATION = 'chess.wsgi.application'
 
 
 # Database
