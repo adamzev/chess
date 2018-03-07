0e704b523674aba89f300a2ec70cde742531efd6
Adam Levin <adamlevin@tutordelphia.com>
2018-03-07 11:13:48 -0500

    refactors homepage to use a template

--- game/views.py
+++ game/views.py
@@ -3,12 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse(
-        '''<html>
-                <head>
-                    <title>Chess Teacher</title>
-                </head>
-                <body>
-                </body>
-            </html>'''
-    )
+    return render(request, 'home.html')
